e188009a1e568120d83add4d28e56aa9c90fccaa
Felipe Lima <lima_felipe@rocketmail.com>
2023-12-08 16:43:36 +0100

    fix: change function names

--- tests/test_processed_data.py
+++ tests/test_processed_data.py
@@ -3,35 +3,40 @@ import pytest
 import os
 
 # Set local directory
-project_path = 'C:\\Users\\lipe_\\Documents\\projets\\credit-scoring\\'
+project_path = '/Users/felipelima/Documents/projets/credit-scoring/'
 os.chdir(project_path)
 
 @pytest.fixture(scope='module')
 def get_train_data():
     '''Get customers processed train data to feed into the tests'''
-    return pd.read_csv('data\\processed\\train_feature_engineering_encoded.csv')
+    return pd.read_csv('data/processed/train_feature_engineering_encoded.csv')
 
 @pytest.fixture(scope='module')
 def get_test_data():
     '''Get customers processed test data to feed into the tests'''
-    return pd.read_csv('data\\processed\\test_feature_engineering_encoded.csv')
+    return pd.read_csv('data/processed/test_feature_engineering_encoded.csv')
 
-def check_train_duplicates(train_data):
+def test_train_duplicates(get_train_data):
     '''Check if the train duplicated dataframe is empty --> no duplicates'''
+    train_data = get_train_data
     duplicates = train_data[train_data.duplicated()]
     assert duplicates.empty
 
-def check_test_duplicates(test_data):
+def test_test_duplicates(get_test_data):
     '''Check if the test duplicated dataframe is empty --> no duplicates'''
+    test_data = get_test_data
     duplicates = test_data[test_data.duplicated()]
     assert duplicates.empty
 
-def check_train_target_col(train_data):
+def test_train_target_col(get_train_data):
     '''Test that the train dataframe has a 'target' column'''
+    train_data = get_train_data
     assert 'TARGET' in train_data.columns
 
-def check_train_test_sizes(train_data, test_data):
+def test_train_test_sizes(get_train_data, get_test_data):
     '''Check that train and test dataframe have the same columns (but target)'''
+    train_data = get_train_data
+    test_data = get_test_data
     train_size = train_data.drop(columns='TARGET').shape[1]
     test_size = test_data.shape[1]
     assert train_size == test_size
